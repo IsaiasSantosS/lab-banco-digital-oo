395164647108d76e03dfae8d48acfea4e706b758
Isaias Santos <isaiassantos@ufc.br>
2025-10-02 11:55:24 -0300

    instalar o lombok e refatorar o código

--- src/main/java/com/isaiassantos/model/Banco.java
+++ src/main/java/com/isaiassantos/model/Banco.java
@@ -1,26 +1,17 @@
 package com.isaiassantos.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Banco {
 
 	private String nome;
 	private List<Conta> contas;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public List<Conta> getContas() {
-		return contas;
-	}
-
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
-	}
 
 }
